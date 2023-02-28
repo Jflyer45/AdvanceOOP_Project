77d8e69e810b7c9d07b8e7adce8fabbcb1f7896d
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2023-02-28 12:36:56 -0600

    Basic BT move to scirpt that can be used for patrolling

---
+++ AdvanceOOP_UnityProject/Assets/Scripts/BT/MoveTo.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine.AI;
+public class MoveTo : Action
+{
+    [SerializeField] protected GameObject location;
+    [SerializeField] protected Speed speed;
+
+    protected NavMeshAgent agent;
+
+    public enum Speed
+    {
+        Walk, Run
+    }
+
+    // Start is called before the first frame update
+    public override void OnAwake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public override void OnStart()
+    {
+        SetUpAnimation();
+    }
+
+    public void SetUpAnimation()
+    {
+        /*
+        if (speed == Speed.Run)
+        {
+            aiRoot.RunAnimation();
+        }
+        else if (speed == Speed.Walk)
+        {
+            aiRoot.WalkAnimation();
+        }
+        */
+    }
+
+    public override TaskStatus OnUpdate()
+    {
+
+        agent.SetDestination(location.transform.position);
+
+        if (!agent.pathPending && !agent.isOnOffMeshLink)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                //aiRoot.IdleAnimation();
+                return TaskStatus.Success;
+            }
+        }
+        return TaskStatus.Running;
+    }
+}
