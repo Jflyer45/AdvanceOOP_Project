15bad945fa6bfacca8c8ff8a1feeae0c54b45cf5
Jeremy Fischer <63757124+Jflyer45@users.noreply.github.com>
2023-03-19 16:00:54 -0500

    Add Singleton to GM

--- AdvanceOOP_UnityProject/Assets/Scripts/GameManger.cs
+++ AdvanceOOP_UnityProject/Assets/Scripts/GameManger.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManger : MonoBehaviour
+
+public class GameManger : Singleton<GameManger>
 {
     // Start is called before the first frame update
     void Start()
